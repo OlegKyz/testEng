508cbb71c4189534f5ca2ad5d5814f261c58f7a3
Oleg Kuznetsov <oleg.kyznetzov@mail.ru>
2022-07-03 11:50:44 +0400

    added main window

--- src/main/java/ru/kuznetsov/oleg/App.java
+++ src/main/java/ru/kuznetsov/oleg/App.java
@@ -1,13 +1,24 @@
 package ru.kuznetsov.oleg;
 
-/**
- * Hello world!
- *
- */
+import java.awt.event.*;
+import java.awt.*;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class App 
 {
+	private final static int mainWindowWidth = 400;
+	private final static int mainWindowHeight = 400;
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.setSize(mainWindowWidth, mainWindowHeight);
+        mainWindow.setTitle("TestEng");
+        mainWindow.setVisible(true);
     }
 }
+
---
+++ src/main/java/ru/kuznetsov/oleg/MainWindow.java
@@ -0,0 +1,105 @@
+package ru.kuznetsov.oleg;
+
+import java.awt.event.*;
+import java.awt.*;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainWindow extends Frame {
+	private final static String irregularTestPath = 
+		"src\\main\\resources\\irregularVerbs";
+	private final static String wordsTestPath = 
+		"src\\main\\resources\\words";
+
+	Panel testsPanel;
+	CardLayout cardL;
+	Button irregularVerbsButton, wordsButton;
+	Button[] irregularVerbsTestsButtons;
+	Button[] wordsTestsButtons;
+
+	public MainWindow() {
+		setLayout(new FlowLayout());
+		irregularVerbsButton = new Button("Irregular verbs");
+		wordsButton = new Button("Words");
+		add(irregularVerbsButton);
+		add(wordsButton);
+
+		cardL = new CardLayout();
+		testsPanel = new Panel();
+		testsPanel.setLayout(cardL);
+
+		Panel irregularVerbsPanel = new Panel();
+		irregularVerbsPanel.setLayout(new GridLayout(0, 5, 10, 10));
+		irregularVerbsTestsButtons = new Button[3];
+		for (int i = 0; i < 3; ++i) {	
+			irregularVerbsTestsButtons[i] = new Button("test # " + i);
+			irregularVerbsPanel.add(irregularVerbsTestsButtons[i]);
+		}
+
+		Panel wordsPanel = new Panel();
+		wordsPanel.setLayout(new GridLayout(0, 5, 10, 10));
+		wordsTestsButtons = new Button[10];
+		for (int i = 0; i < 10; ++i) {
+			wordsTestsButtons[i] = new Button("test № " + i);
+			wordsPanel.add(wordsTestsButtons[i]);
+		}
+
+		testsPanel.add(irregularVerbsPanel, "Irregular Verbs");
+		testsPanel.add(wordsPanel, "Words");
+
+		add(testsPanel);
+
+		irregularVerbsButton.addActionListener((ae) ->
+			cardL.show(testsPanel, "Irregular Verbs"));
+
+		wordsButton.addActionListener((ae) ->
+			cardL.show(testsPanel, "Words"));
+
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we) {
+				System.exit(0);
+			}
+		});
+		// File irregularVerbsTestDir = new File(irregularTestPath);
+		// File wordsTestDir = new File(wordsTestPath);
+		
+		// printTestFiles(irregularVerbsTestDir, wordsTestDir);
+	}
+
+	private void printTestFiles(File irregularVerbsTestDir,
+		File wordsTestDir) {
+		try {
+			File[] irregularVerbsTestFiles = 
+				irregularVerbsTestDir.listFiles();
+			for (File buf : irregularVerbsTestFiles) {
+				System.out.println(buf.getName());
+				BufferedReader reader = new BufferedReader(new FileReader(buf));
+				String line = reader.readLine();
+				while (line != null) {
+					System.out.println("----" + line);
+					line = reader.readLine();
+				}
+			}
+			System.out.println();
+			
+			File[] wordsTestFiles = wordsTestDir.listFiles();
+			for (File buf : wordsTestFiles) {
+				System.out.println(buf.getName());
+				BufferedReader reader = new BufferedReader(new FileReader(buf));
+				String line = reader.readLine();
+				while (line != null) {
+					System.out.println("----" + line);
+					line = reader.readLine();
+				}
+			}
+			System.out.println();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
